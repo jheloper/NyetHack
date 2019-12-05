7c5d846d282ebe35ab256d028ef01f0075530dcd
Joonghyeon Kim <jheloper9102@gmail.com>
2019-12-05 17:18:34 +0900

    논리 연산자를 통해 캐릭터가 축복받은 경우(isBlessed)와 불멸자인 경우(isImmortal)에 대한 처리 추가

--- src/Game.kt
+++ src/Game.kt
@@ -5,13 +5,26 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false;
+
+    // aura
+    if (isBlessed && healthPoints > 50 || isImmortal) {
+        println(name + " aura is green")
+    } else {
+        println(name + " aura is none")
+    }
 
     if (healthPoints == 100) {
         println(name + " best condition!")
     } else if (healthPoints >= 90) {
         println(name + " some scratch.")
     } else if (healthPoints >= 75) {
-        println(name + " slight wound.")
+        if (isBlessed) {
+            println(name + " slight wound, but heal quickly.")
+        } else {
+            println(name + " slight wound.")
+        }
     } else if (healthPoints >= 15) {
         println(name + " hurt a lot.")
     } else {
