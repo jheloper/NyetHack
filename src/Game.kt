b309411d4f17ac986eca05c8e0bc9fe91f259ee0
Joonghyeon Kim <jheloper9102@gmail.com>
2019-12-10 17:17:16 +0900

    조건 표현식을 통해 중복되는 코드 수정

--- src/Game.kt
+++ src/Game.kt
@@ -6,28 +6,27 @@ fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoints = 89
     val isBlessed = true
-    val isImmortal = false;
+    val isImmortal = false
 
     // aura
-    if (isBlessed && healthPoints > 50 || isImmortal) {
-        println(name + " aura is green")
-    } else {
-        println(name + " aura is none")
-    }
+    val auraInvisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraInvisible) "green" else "none"
+    println(name + " aura is " + auraColor)
 
-    if (healthPoints == 100) {
-        println(name + " best condition!")
+    val healthStatus = if (healthPoints == 100) {
+        "best condition!"
     } else if (healthPoints >= 90) {
-        println(name + " some scratch.")
+        "some scratch."
     } else if (healthPoints >= 75) {
         if (isBlessed) {
-            println(name + " slight wound, but heal quickly.")
+            "slight wound, but heal quickly."
         } else {
-            println(name + " slight wound.")
+            "slight wound."
         }
     } else if (healthPoints >= 15) {
-        println(name + " hurt a lot.")
+        "hurt a lot."
     } else {
-        println(name + " worst condition!")
+        "worst condition!"
     }
+    println(name + " " + healthStatus)
 }
