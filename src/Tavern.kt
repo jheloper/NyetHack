8ae99d69d6d921cdfb270de59f7f880ad30a99d7
Joonghyeon Kim <jheloper9102@gmail.com>
2020-03-05 16:52:07 +0900

    Tavern 내부 코드들을 별개의 함수로 분리 및 if로 null 검사, null 복합 연산자(엘비스 연산자) 예제 코드 추가

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -3,8 +3,16 @@
  */
 
 fun main(args: Array<String>) {
-    var signatureDrink = "Beer"
+    val signatureDrink = "Beer"
 
+    // orderBeverage1(signatureDrink)
+
+    // orderBeverage2()
+
+    orderBeverage3(signatureDrink)
+}
+
+fun orderBeverage1(signatureDrink: String) {
     // 아래와 같이 안전 호출 연산자(?.)를 이용하면 반환값이 null이 아닌 경우에만 체이닝된 함수를 호출한다.
     var beverage = readLine()?.let {
         // let 함수로 해당 값이 null이 아닌 경우에 대한 처리를 추가할 수 있다.
@@ -15,6 +23,27 @@ fun main(args: Array<String>) {
         }
     }
     // beverage = null
-
     println(beverage)
+}
+
+fun orderBeverage2() {
+    // 더블 뱅 연산자(=non null 단언 연산자)로 null이 될 수 없음을 단언할 수 있다.
+    var beverage = readLine()!!.capitalize()
+    println(beverage)
+}
+
+fun orderBeverage3(signatureDrink: String) {
+    // 아래처럼 if로 검사할 수 있다.
+    var beverage = readLine()
+    beverage = null
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("beverage is null")
+    }
+    println(beverage)
+
+    // null 복합 연산자(=Elvis 연산자)를 사용하여 null인 경우와 null이 아닌 경우에 대해서 처리할 수 있다.
+    val beverageServed: String = beverage ?: signatureDrink
+    println(beverageServed)
 }
