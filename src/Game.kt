2ed780c59005aec1a67544a9fa56930d6ed78dff
Joonghyeon Kim <jheloper9102@gmail.com>
2020-05-03 19:43:50 +0900

    Player 클래스 및 Game 파일 코드 리팩터링

--- src/Game.kt
+++ src/Game.kt
@@ -3,10 +3,10 @@ const val MAX_EXPERIENCE: Int = 5000
 
 fun main(args: Array<String>) {
     // 지역 변수
-    var healthPoints = 89
-    val isBlessed = true
-    val isImmortal = false
-    val race = "gnome"
+    // var healthPoints = 89
+    // val isBlessed = true
+    // val isImmortal = false
+    // val race = "gnome"
 
     val player = Player()
     player.name = "estragon "
@@ -14,22 +14,8 @@ fun main(args: Array<String>) {
 
     player.castFireBall()
 
-    // faction
-    val faction = faction(race)
-
-    // aura
-    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
-
     // 지명 함수 인자를 사용하여 원하는 순서로 인자를 전달할 수 있다.
-    printPlayerStatus(
-        healthPoints = healthPoints,
-        auraColor = auraColor,
-        isBlessed = isBlessed,
-        name = player.name,
-        healthStatus = healthStatus,
-        faction = faction
-    )
+    printPlayerStatus(player)
 
     performCombat()
     performCombat("Ulrich")
@@ -42,71 +28,25 @@ fun main(args: Array<String>) {
     println(myDice.rolledValue)
 }
 
-private fun faction(race: String): String {
-    return when (race) {
-        "dwarf" -> "Keepers of the Mines"
-        "gnome" -> "Keepers of the Mines"
-        "orc" -> "Free People of the Rolling Hills"
-        "human" -> "Free People of the Rolling Hills"
-        else -> "No faction"
-    }
-}
-
 private fun printPlayerStatus(
-    healthPoints: Int,
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String,
-    faction: String
+    player: Player
 ) {
-    println("$name faction is $faction")
-    println("(HP: $healthPoints)(Aura: $auraColor)(Blessed: ${if (isBlessed) "YES" else "NO"}) -> $name $healthStatus")
-    // castFireBall()
+    println("${player.name} faction is ${player.faction()}")
+    println("(HP: ${player.healthPoints})(Aura: ${player.auraColor()})(Blessed: ${if (player.isBlessed) "YES" else "NO"}) -> ${player.name} ${player.formatHealthStatus()}")
 }
-
-//private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
-//    val auraInvisible = isBlessed && healthPoints > 50 || isImmortal
-//    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20).toInt()
-//    val auraColor = if (auraInvisible) {
-//        when (karma) {
-//            in 0..5 -> "red"
-//            in 6..10 -> "orange"
-//            in 11..15 -> "purple"
-//            in 16..20 -> "green"
-//            else -> "none"
-//        }
-//    } else {
-//        "none"
-//    }
-//    return auraColor
-//}
-
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
-    if (isBlessed && healthPoints > 50 || isImmortal) {
-        when ((Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20).toInt()) {
-            in 0..5 -> "red"
-            in 6..10 -> "orange"
-            in 11..15 -> "purple"
-            in 16..20 -> "green"
-            else -> "none"
-        }
-    } else {
-        "none"
-    }
 
 // 단일 표현식 함수. 함수 몸체를 사용하는 대신 대입 연산자 다음에 표현식(실행 코드) 정의.
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
-    100 -> "best condition!"
-    in 90..99 -> "some scratch."
-    in 75..89 -> if (isBlessed) {
-        "slight wound, but heal quickly."
-    } else {
-        "slight wound."
-    }
-    in 15..74 -> "hurt a lot."
-    else -> "worst condition!"
-}
+//private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
+//    100 -> "best condition!"
+//    in 90..99 -> "some scratch."
+//    in 75..89 -> if (isBlessed) {
+//        "slight wound, but heal quickly."
+//    } else {
+//        "slight wound."
+//    }
+//    in 15..74 -> "hurt a lot."
+//    else -> "worst condition!"
+//}
 
 // 아무것도 반환하지 않는 함수는 Unit 반환 함수.
 //private fun castFireBall(numFireBalls: Int = 2) = println("shoot fire ball. (x$numFireBalls)")
--- src/Player.kt
+++ src/Player.kt
@@ -4,6 +4,45 @@ class Player {
         set(value) {
             field = value.trim()
         }
+    val race = "gnome"
+    var healthPoints = 89
+    val isBlessed = true
+    private val isImmortal = false
+
+    fun auraColor(): String {
+        val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+        return if (auraVisible) selectAuraColor() else "NONE"
+    }
+
+    private fun selectAuraColor(): String {
+        return when ((Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20).toInt()) {
+            in 0..5 -> "RED"
+            in 6..10 -> "ORANGE"
+            in 11..15 -> "PURPLE"
+            in 16..20 -> "GREEN"
+            else -> "NONE"
+        }
+    }
+
+    fun formatHealthStatus() = when (healthPoints) {
+        100 -> "best condition!"
+        in 90..99 -> "some scratch."
+        in 75..89 -> if (isBlessed) {
+            "slight wound, but heal quickly."
+        } else {
+            "slight wound."
+        }
+        in 15..74 -> "hurt a lot."
+        else -> "worst condition!"
+    }
+
+    fun faction() = when (race) {
+        "dwarf" -> "Keepers of the Mines"
+        "gnome" -> "Keepers of the Mines"
+        "orc" -> "Free People of the Rolling Hills"
+        "human" -> "Free People of the Rolling Hills"
+        else -> "No faction"
+    }
 
     fun castFireBall(numFireBalls: Int = 2) = println("shoot fire ball. (x$numFireBalls)")
 }
